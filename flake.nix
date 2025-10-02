fb570c6bd5a1fc719aac49e9c2357ff5f50af99d
cheezecakee <anabel.pmoraes@gmail.com>
2025-10-02 11:15:38 -0300

    feat(flake): add initial flake configuration

--- flake.nix
+++ flake.nix
@@ -4,13 +4,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
+    lanzaboote.url  =  "github:nix-community/lanzaboote/v0.4.2";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: {
+  outputs = { self, nixpkgs, lanzaboote, flake-utils, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./hosts/default.nix
+        lanzaboote.nixosModules.lanzaboote
+        ./hosts/desktop/default.nix
       ];
     };
   };
